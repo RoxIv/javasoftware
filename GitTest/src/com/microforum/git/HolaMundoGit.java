ecce74eb27e251edc21b45d7b7ee62ef7d09ddcd
microrocio <microrocio@gmail.com>
2013-02-20 09:25:01 +0100

    Commit Inicial

---
+++ GitTest/src/com/microforum/git/HolaMundoGit.java
@@ -0,0 +1,13 @@
+package com.microforum.git;
+
+public class HolaMundoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Se va a enviar a GIT");
+
+	}
+
+}
